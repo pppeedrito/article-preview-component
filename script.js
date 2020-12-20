4797c55d2cff5fd030d659b5b36bda27a0afbd0a
Itai Mapaya <mambostudioz@gmail.com>
2020-12-20 10:28:31 -0600

    Add functionality

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 /* *****variables***** */
+const btnShare = document.querySelector("div.btn-share-container");
+const socialMediaOptions = document.querySelector("div.social-media");
 
 /* *****functions***** */
+function toggleShareOptions() {
+  // add class 'share-article' to the div with class 'author-share'
+  document
+    .querySelector("div.author-share-container")
+    .classList.toggle("share-article");
+}
 
 /* *****event listeners***** */
+// display share options
+btnShare.addEventListener("click", toggleShareOptions);
+
+//simulate sharing link
+socialMediaOptions.addEventListener("click", (e) => {
+  if (e.target.classList.contains("social-media-icon")) {
+    toggleShareOptions();
+  }
+});
