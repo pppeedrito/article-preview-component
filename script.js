e0b8d1befb1b39d723cc465cee3be4a00f646f17
pedro nin <pedro1996616@gmail.com>
2020-12-21 10:13:14 +0100

    FIN

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-/* *****variables***** */
-const btnShare = document.querySelector("div.btn-share-container");
-const socialMediaOptions = document.querySelector("div.social-media");
 
-/* *****functions***** */
-function toggleShareOptions() {
-  // add class 'share-article' to the div with class 'author-share'
-  document
-    .querySelector("div.author-share-container")
-    .classList.toggle("share-article");
+//variables
+const btn = document.querySelector("#btn_share");
+const iconSocial = document.querySelector("div.social-media");
+
+function toogleShare(){
+
+document.querySelector("div.author-share-container").classList.toggle("share-article");
 }
 
-/* *****event listeners***** */
-// display share options
-btnShare.addEventListener("click", toggleShareOptions);
+btn.addEventListener("click", toogleShare);
 
-//simulate sharing link
-socialMediaOptions.addEventListener("click", (e) => {
-  if (e.target.classList.contains("social-media-icon")) {
-    toggleShareOptions();
-  }
-});
+//hover sharing link 
+iconSocial.addEventListener("click", (e)=>{
+if (e.target.classList.contains("social-media-icon")){
+  toogleShare();
+}
+})
